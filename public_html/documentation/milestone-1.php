cef0a230b1d513a72384d72d4b5f9e5772a122df
Jamie Amparan <jleespooks@gmail.com>
2019-04-18 08:15:41 -0600

    had to abandon original pwp jleespooks-pwp because of issues that couldnt be resolved. This is the new/complete pwd that is working.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Jleespooks-PWP</title>
+	</head>
+	<h1>Purpose, Audience and Goal</h1>
+	<body>
+		<b>Purpose:</b>
+		<p>My professional portfolio showcasing my capabilities and skills as a Junior web developer showing that Im available for hire.</p>
+		<b>Audience:</b>
+		<p>Professional audience/Other Devs. Local recruiter from an expanding tech company.  </p>
+		<b>Goal:</b>
+		<p>The goal of my site is to attract job recruiters and/or clients and collaborators, while showcasing my work, city, and skills. Also have them be able to contact me through a contact form/email/linkedIn/Github on a simple to read page.  </p>
+		<br>
+
+		<h2>Persona:</h2>
+		<p>
+			<b>Name:</b>
+			Jen Berry
+		</p>
+
+		<p>
+			<b>Age:</b>
+			39
+		</p>
+
+		<p>
+			<b>Profession:</b>
+			Web manager of expanding tech company, who is working together with a team of senior web developers, is looking to hire local Junior web developers full-time. They recently relocated and are in need of local interns.
+		</p>
+
+		<p>
+			<b>Technology:</b>
+		<ul>
+			<li>At work: CPU: 2.5GHz Intel Core i7-6500U/ Graphics: Intel HD Graphics 520/RAM: 16GB/Screen: 13.3-inch, 1920 x 1080 pixels/Storage: 512GB SSD</li>
+			<li>For home: HP 255 G5 laptop/CPU: AMD A6-7310/Graphics: AMD Radeon R4/RAM: 4GB/Storage: 1TB HDD</li>
+			<li>Phone: Samsung Galaxy s10+</li>
+		</ul>
+		<p>
+
+		<p>
+			<b>Attitudes and Behaviors:</b>
+			New to town and pretty stressed about the new project under way. Doesn't want to waste the day reading through hundreds of lengthy resumes to find what she needs. Wants to see what new candidate is capable of. Uses linkeIn and github for references.
+		</p>
+
+		<p>
+			<b>Frustrations and Needs:</b>
+			Jen needs to hire a junior web developer. Some experience is optional. Candidate must be local and determined.
+		</p>
+
+		<p>
+			<b>Goals:</b>
+			Jen's goals are to be able to find a local junior web developer by quickly glancing at a page and quickly get in touch.
+		</p>
+
+		<p>
+			<b>User Story:</b>
+			As a user, I need to be able to determine whether this candidate is local and a junior web developer.
+		</p>
+
+	</body>
+</html>
