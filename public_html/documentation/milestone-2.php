0cc7ba046373c2036102a8604fcd3bc7e77530e4
Jamie Amparan <jleespooks@gmail.com>
2019-04-30 08:03:23 -0600

    Milestone 2 start

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Content Strategy</title>
+	</head>
+	<body>
+		<b>Welcome:</b>
+		<p>Jamie Amparan Full Stack Web Developer</p>
+		<b>About Me:</b>
+		<p></p>
+
+	</body>
+</html>
