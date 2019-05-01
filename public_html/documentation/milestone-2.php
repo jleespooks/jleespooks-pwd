0315ea3a873f9154006765e2023fbab155a022c9
Jamie Amparan <jleespooks@gmail.com>
2019-05-01 13:15:00 -0600

    Milestone 2 adding the wireframes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -2,13 +2,21 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Content Strategy</title>
+		<title>Milestone 2</title>
 	</head>
 	<body>
-		<b>Welcome:</b>
-		<p>Jamie Amparan Full Stack Web Developer</p>
-		<b>About Me:</b>
-		<p></p>
+		<h1>Content Strategy</h1>
+		<p>My site will be easy to use and on one page.</p>
 
+		<b>Welcome:</b>
+		<p>Jamie Amparan, Full Stack Web Developer.  Displayed on a large image of ABQ/Balloons/or Downtown at night.</p>
+		<b>About Me:</b>
+		<p>In my about me section I will have a professional headshot next to a container full of info. I will list my skills as a Web developer. I will show my personality through this section.</p>
+		<b>Portfolio:</b>
+		<p> This section will show projects that I have finished and have been involved in. I would like to have this section animated in some way whether it be an ipod style flip book or on a timer.</p>
+		<b>Contact:</b>
+		<p>This section will have the email form as well as links to my github, linkedin, workphone, and maybe a business instagram </p>
+		<img src="milestone-1.php">
+		<img src="milestone-2.php">
 	</body>
 </html>
